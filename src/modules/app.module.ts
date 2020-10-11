6ef489faa03be676e7592192e6e8b4ab01cf91f3
thiagodantasv <thiagodantasv@gmail.com>
2020-10-11 19:42:52 -0300

    user module. Multiple modules in being imported on one root module

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { BooksModule } from './books.module'; 
+import { UserModule } from './user.module';
 
 @Module({
-  imports: [BooksModule],
+  imports: [BooksModule, UserModule],
   controllers: [],
   providers: [],
   exports: []
